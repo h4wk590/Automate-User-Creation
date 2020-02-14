eb75ce27a56320145d680885c8735e9c4849bdd1
Aidan Brown <da-browna@THESHIRE.com>
2020-02-14 15:38:58 -0800

    xlsx to csv

--- 280_AB.ps1
+++ 280_AB.ps1
@@ -1,1 +1,18 @@
-﻿
+﻿Import-Module ActiveDirectory
+
+Function ExcelCSV ($File)
+{
+ 
+    $excelFile = "c:\project\" + $File + ".xlsx"
+    $Excel = New-Object -ComObject Excel.Application
+    $Excel.Visible = $false
+    $Excel.DisplayAlerts = $false
+    $wb = $Excel.Workbooks.Open($excelFile)
+    foreach ($ws in $wb.Worksheets)
+    {
+        $ws.SaveAs("c:\project" + $File + ".csv", 6)
+    }
+    $Excel.Quit()
+}
+$FileName = "Users"
+ExcelCSV -File "$FileName"
