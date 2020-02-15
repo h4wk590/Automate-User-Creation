7d94c8a91ea0b33300991b392a6342c5f9378657
administrator@THESHIRE.COM <administrator@THESHIRE.COM>
2020-02-15 13:30:10 -0800

    start adUser add function

--- 280_AB.ps1
+++ 280_AB.ps1
@@ -36,11 +36,30 @@ ExcelCSV -File "$FileName"
 
 
 
+# This function creates 6 groups in the domain 
+Function createGroups
+{
+    # Array containing the group names
     $groupNames = "Group1", "Group2", "Group3", "Group4", "Group5", "Group6"
+
+    # Loop through the array and create a new OU per array object
     foreach ($OU in $groupNames)
     {
         New-ADOrganizationalUnit -Name $OU -Path "OU=groups,DC=THESHIRE,DC=COM"
     }
                      
- 
-         write-host "Creating OUs for $groupNames"
+        write-host "Creating OUs for $groupNames"
+}
+
+
+Function addUsers
+
+{
+    
+    $adUsers = Import-Csv C:\*.csv
+
+    foreach ($user in $adUsers)
+    {
+
+    }
+}
