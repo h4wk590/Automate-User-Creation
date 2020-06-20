d9e64d13b75d324c917ea5717a57fa8753f3bc1e
h4wk590 <45271332+h4wk590@users.noreply.github.com>
2020-06-19 22:10:45 -0700

    Update create_groups.ps1

--- create_groups.ps1
+++ create_groups.ps1
@@ -1,8 +1,7 @@
 ﻿#create 6 groups in Active Directory
-
+ 
     # Array containing the group names
     $groupNames = "Group1", "Group2", "Group3", "Group4", "Group5", "Group6"
-
     # Loop through the array and create a new OU per array object
     foreach ($OU in $groupNames)
     {
